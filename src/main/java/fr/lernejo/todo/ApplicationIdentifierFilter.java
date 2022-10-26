3af6bfb677ab5289f438978b9c71337a3842aadf
Gregory L <gregoryslaurent@gmail.com>
2022-10-26 16:22:16 +0200

    partie 4

---
+++ src/main/java/fr/lernejo/todo/ApplicationIdentifierFilter.java
@@ -0,0 +1,36 @@
+package fr.lernejo.todo;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class ApplicationIdentifierFilter implements Filter {
+
+    String uuid = UUID.randomUUID().toString();
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+
+        /** on appelle le filtre suivant en lui passant servletRequest
+            et il récupère ensuite le return du filtre suivant
+         *          x --------> F1 --------> F2 --------> Servlet
+         *            <-------     <--------    <--------
+         */
+
+        if (servletResponse instanceof HttpServletResponse httpResponse) {
+            httpResponse.setHeader("Instance-Id", uuid);
+            /* revient à : ((HttpServletResponse) servletResponse).addHeader...
+             mais sans cast */
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+
+
+    }
+}
